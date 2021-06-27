adfcf3baa960fb1ca245a8b7f6bd15e7bc9a1576
Bibi <84073518+xBibie@users.noreply.github.com>
2021-06-27 11:55:20 +0200

    Add files via upload

---
+++ opdracht_010/test.js
@@ -0,0 +1,49 @@
+const age= 18;
+const isFemale = Boolean;
+const driverStatus = bob;
+const firstName = Sarah || Bram;
+const totalAmount = 60;
+
+/*
+Testdata
+const age = 16; 
+const isFemale = false;
+const driverStatus = 808; 
+*/
+
+if (age > = 18) {
+    console.log('you are 18+, you can have come in');
+} else {
+    console.log('too young, too bad');
+}
+
+if (isFemale) {
+    console.log ('yay ladies');
+} else {
+    console.log('Boo, beer for you');
+}
+
+if (driverStatus === 'bob') {
+    console.log ('you can drive buddy');
+} else {
+    console.log ('too bad buddy');
+}
+if (age >=18 && <= 25) {
+    console.log('Yay! 50% korting!');
+} else {
+    console.log ('Full price');
+}
+
+if (firstName Sarah || Bram) {
+    console.log('Yay! Gratis biertje :)');
+} else {
+    console.log ('Geen gratis biertje :(');
+}
+
+if (totalAmount >25){
+    console.log ('Gratis (vega)bitterballen!');
+} else if (totalAmount >50) {
+    console.log ('Gratis nacho's!);
+} else if (totalAmount >100);{
+    console.log ('Gratis fles champie!');
+}
